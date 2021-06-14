484a212295cf0b0aead9f6d3cfe5a202f6425b28
Drinnn <pedro_lontro@hotmail.com>
2021-06-14 10:32:43 -0300

    feat: add product enable implementation

--- application/product.go
+++ application/product.go
@@ -1,5 +1,7 @@
 package application
 
+import "errors"
+
 type ProductInterface interface {
 	IsValid() (bool, error)
 	Enable() error
@@ -22,30 +24,35 @@ type Product struct {
 	Status string
 }
 
-func (p *Product) IsValid() (bool, error) {
+// func (p *Product) IsValid() (bool, error) {
 
-}
+// }
 
 func (p *Product) Enable() error {
+	if p.Price > 0 {
+		p.Status = ENABLED
+		return nil
+	}
 
+	return errors.New("price must be greater than 0 to enable the product")
 }
 
-func (p *Product) Disable() error {
+// func (p *Product) Disable() error {
 
-}
+// }
 
 func (p *Product) GetId() string {
-
+	return p.Id
 }
 
 func (p *Product) GetName() string {
-
+	return p.Name
 }
 
 func (p *Product) GetPrice() float64 {
-
+	return p.Price
 }
 
 func (p *Product) GetStatus() string {
-
+	return p.Status
 }
