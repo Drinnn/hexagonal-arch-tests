3135af0cf9d62799efe8a9d2ff80d9e8ee458222
Drinnn <pedro_lontro@hotmail.com>
2021-06-16 14:50:21 -0300

    test: add product disable and isValid tests

--- application/product_test.go
+++ application/product_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 
 	"github.com/Drinnn/hexagonal-arch-tests/application"
+	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/require"
 )
 
 func TestProduct_Enable(t *testing.T) {
 	product := application.Product{}
+	product.Id = uuid.NewV4().String()
 	product.Name = "Hello"
 	product.Status = application.DISABLED
 	product.Price = 10
@@ -19,4 +21,41 @@ func TestProduct_Enable(t *testing.T) {
 	product.Price = 0
 	err = product.Enable()
 	require.Equal(t, "price must be greater than 0 to enable the product", err.Error())
+}
+
+func TestProduct_Disable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.ENABLED
+	product.Price = 0
+
+	err := product.Disable()
+	require.Nil(t, err)
+
+	product.Price = 10
+	err = product.Disable()
+	require.Equal(t, "price must be 0 to disable the product", err.Error())
+}
+
+func TestProduct_IsValid(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+	product.Id = ""
+
+	_, err := product.IsValid()
+	require.Nil(t, err)
+
+	product.Status = "INVALID"
+	_, err = product.IsValid()
+	require.Equal(t, "status must be enabled or disabled", err.Error())
+
+	product.Status = application.ENABLED
+	_, err = product.IsValid()
+	require.Nil(t, err)
+
+	product.Price = -10
+	_, err = product.IsValid()
+	require.Equal(t, "price must be greater than or equal 0", err.Error())
 }
